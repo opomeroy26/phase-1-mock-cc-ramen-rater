00c853f4dc305c188955d88863c7f5ecec694172
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-09 11:37:25 -0800

    first deliverable modified

--- src/index.js
+++ src/index.js
@@ -15,13 +15,30 @@ getData();
 
 function arrayObj(data) {
         const ramenMenu = document.querySelector("#ramen-menu")
-        const img = document.createElement("img")
-        img.src = "http://localhost:3000/ramens/1"
-        ramenMenu.appendChild(img)
-        img.innerHTML = data[0].image
-        ramenMenu.append(img)
+        const img1 = document.createElement("img")
+        img1.src = "./assets/ramen/shoyu.jpg"
+        ramenMenu.appendChild(img1)
 
-        console.log(img)
+        const img2 = document.createElement("img")
+        img2.src = "./assets/ramen/naruto.jpg"
+        ramenMenu.appendChild(img2)
+
+        const img3 = document.createElement("img")
+        img3.src = "./assets/ramen/nirvana.jpg"
+        ramenMenu.appendChild(img3)
+
+        const img4 = document.createElement("img")
+        img4.src = "./assets/ramen/gyukotsu.jpg"
+        ramenMenu.appendChild(img4)
+
+        const img5 = document.createElement("img")
+        img5.src = "./assets/ramen/kojiro.jpg"
+        ramenMenu.appendChild(img5)
+       
+       // img.innerHTML = data[0].image
+        //ramenMenu.append(img)
+
+       // console.log(img)
        
 
     }
