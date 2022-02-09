246a0dd2073d374d3ca9f372db99627f6f2a97de
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-09 11:28:24 -0800

    first deliverable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 // write your code here
+
+const url = "http://localhost:3000/ramens"
+
+function getData() {
+ fetch (url)
+.then (resp => resp.json())
+.then (data => 
+   (arrayObj(data)))
+   //console.log (data))
+}
+getData();
+    
+
+
+function arrayObj(data) {
+        const ramenMenu = document.querySelector("#ramen-menu")
+        const img = document.createElement("img")
+        img.src = "http://localhost:3000/ramens/1"
+        ramenMenu.appendChild(img)
+        img.innerHTML = data[0].image
+        ramenMenu.append(img)
+
+        console.log(img)
+       
+
+    }
+  //getData();
+
